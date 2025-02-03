b0ad5205fb2296c14492d38ff38e5b0d05d28ca7
barbercheff <barbercheffdev@gmail.com>
2025-02-03 02:13:03 +0100

    First commit

---
+++ src/main/java/com/drawquest/DrawquestApplication.java
@@ -0,0 +1,13 @@
+package com.drawquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawquestApplication.class, args);
+	}
+
+}
