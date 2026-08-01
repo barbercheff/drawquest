4665b9f29d395fc1e61a73aa5af294e0db9cbfc4
barbercheff <barbercheffdev@gmail.com>
2026-08-01 12:34:47 +0200

    Remove JsonIgnore from user id

--- src/main/java/com/drawquest/models/User.java
+++ src/main/java/com/drawquest/models/User.java
@@ -1,6 +1,5 @@
 package com.drawquest.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -27,7 +26,6 @@ public class User extends AuditableEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonIgnore
     private Long id;
 
     @Column(nullable = false, unique = true)
